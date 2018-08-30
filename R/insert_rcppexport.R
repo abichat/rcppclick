df5b3811e656bf35b2a44842115129742afbc1c2
abichat <a.bichat@yahoo.fr>
2018-08-30 14:08:29 +0200

    Remove unused functions

--- R/insert_rcppexport.R
+++
@@ -1,9 +0,0 @@
-
-#' Insert a Rcpp::export command.
-#' @description Insert \code{// [[Rcpp::export]]}.
-#' @import rstudioapi
-insert_rcppexport <- function(){
-  text <- "// [[Rcpp::export]]\n"
-  range <- document_range(c(1, 0), c(1, 0))
-  insertText(range, text)
-}
