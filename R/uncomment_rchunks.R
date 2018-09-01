070600e0c6ab9dc2d2b9defa7b542627a7b0c902
abichat <a.bichat@yahoo.fr>
2018-09-01 09:04:57 +0200

    Add function uncomment

---
+++ R/uncomment_rchunks.R
@@ -0,0 +1,31 @@
+#' Uncomment all R chunks.
+#' @description Uncomment all commented R chunks present in the current document.
+#' @details Does not uncomment anything if some chunks are opened but not closed (and reciprocally).
+#' @import rstudioapi
+#' @importFrom stringr str_which
+uncomment_rchunks <- function(){
+
+  script <- getActiveDocumentContext()$contents
+  list <- get_chunks_lines(script)
+  lines_commented1 <- str_which(script, "^#")
+  lines_commented2 <- str_which(script, "^# ")
+  lines_commented1 <- setdiff(lines_commented1, lines_commented2)
+
+  if (!is.null(list)) {
+
+    lines <- unlist(lapply(list, function(x) x[-c(1, length(x))]))
+    lines_to_uncomment1 <- intersect(lines, lines_commented1)
+    lines_to_uncomment2 <- intersect(lines, lines_commented2)
+
+    if(length(lines_to_uncomment1) > 0){
+
+      rng1 <- Map(c, lines_to_uncomment1, 1, lines_to_uncomment1, 2)
+      modifyRange(rng1, "")
+    }
+    if(length(lines_to_uncomment2) > 0){
+
+      rng2 <- Map(c, lines_to_uncomment2, 1, lines_to_uncomment2, 3)
+      modifyRange(rng2, "")
+    }
+  }
+}
