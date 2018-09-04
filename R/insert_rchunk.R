2449a2de208b6f45f1c915213cc74c256f98e997
abichat <a.bichat@yahoo.fr>
2018-09-04 18:45:48 +0300

    insert_*() functions work

--- R/insert_rchunk.R
+++ R/insert_rchunk.R
@@ -5,9 +5,18 @@
 #'
 #' \code{*/}
 #' @import rstudioapi
-#'
+#' @importFrom stringr str_detect
 insert_rchunk <- function(){
-  text <- "/*** R\n\n*/\n"
-  range <- document_range(c(1, 0), c(1, 0))
+
+  line <- getActiveDocumentContext()$selection[[1]]$range$start[1]
+  text_line <- getActiveDocumentContext()$contents[line]
+
+  if(str_detect(text_line, "^(\\h)*$")){ # If line is "" or horizontal whitespace
+    text <- "/*** R\n\n*/"
+  } else {
+    text <- "/*** R\n\n*/\n"
+  }
+
+  range <- document_range(c(line, 0), c(line, 0))
   insertText(range, text)
 }
--- R/insert_rcppexport.R
+++ R/insert_rcppexport.R
@@ -1,8 +1,18 @@
 #' Insert a Rcpp::export command.
 #' @description Insert \code{// [[Rcpp::export]]}.
 #' @import rstudioapi
+#' @importFrom stringr str_detect
 insert_rcppexport <- function(){
-  text <- "// [[Rcpp::export]]\n"
-  range <- document_range(c(1, 0), c(1, 0))
+
+  line <- getActiveDocumentContext()$selection[[1]]$range$start[1]
+  text_line <- getActiveDocumentContext()$contents[line]
+
+  if(str_detect(text_line, "^(\\h)*$")){ # If line is "" or horizontal whitespace
+    text <- "// [[Rcpp::export]]"
+  } else {
+    text <- "// [[Rcpp::export]]\n"
+  }
+
+  range <- document_range(c(line, 0), c(line, 0))
   insertText(range, text)
 }
